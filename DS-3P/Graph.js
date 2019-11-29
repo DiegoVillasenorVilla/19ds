e02e813a0b6089bfd18d741d1630f5732686ea1b
DiegoVillasenorVilla <50335824+DiegoVillasenorVilla@users.noreply.github.com>
2019-11-29 09:36:37 -0600

    Add files via upload

--- DS-3P/Graph.js
+++ DS-3P/Graph.js
@@ -1,6 +1,6 @@
 class Graph {
     constructor() {
-        this.aristas= {};
+        this.arista = {};
         this.vertices = [];
         this.numeroAristas = 0;
         this.numeroVertices = 0;
@@ -9,40 +9,42 @@ class Graph {
     }
 
     addVertex(vertice) {
-        this.vertices.push(vertice);  //push para agregarle valores
-        this.aristas[vertice] = [];   //se crea un arreglo
-        this.numeroVertices++;        // va aumentando el numero de vertices con el ++
-    } 
+        this.vertices.push(vertice);
+        this.arista[vertice] = [];
+        this.numeroVertices++;
+    }
 
     addEdge(v1, v2) {
-        this.aristas[v1].push({
+        this.arista[v1].push({
             vertice: v2
         });
-        this.aristas[v2].push({
+        this.arista[v2].push({
             vertice: v1
         });
-        if(!this.conjuntos[this.numeroIndices]){this.conjuntos[this.numeroIndices]=[];
-            this.conjuntos[this.numeroIndices][0]=v1;
-            this.conjuntos[this.numeroIndices][1]=v2;}
-        this.numeroIndices++;
+        //extra  
+        if(!this.conjuntos[this.numeroAristas]){this.conjuntos[this.numeroAristas]=[];
+            this.conjuntos[this.numeroAristas][0]=v1;
+            this.conjuntos[this.numeroAristas][1]=v2;}
+        this.numeroAristas++;
         if(v1===v2){
             this.multigrafo=true;
         }
+
     }
 
 
     printGraph() {
-        let g = "";
+        let graph = "";
         this.vertices.forEach(vertice => {
-            g += vertice + " -> " + this.aristas[vertice].map(n => n.vertice).join("-") + "\n";
+            graph += vertice + " -> " + this.arista[vertice].map(n => n.vertice).join("-") + "\n";
         });
-        console.log(g);
+        console.log(graph);
     }
 
     isComplete() {
         let R = false;
-        let aux = (this.numeroVertices * (this.numeroVertices - 1)) / 2;
-        if (this.numeroAristas === aux) {
+        let A = (this.numeroVertices * (this.numeroVertices - 1)) / 2;
+        if (this.numeroAristas === A) {
             R = true;
         } else R = false
 
@@ -57,27 +59,27 @@ class Graph {
     }
 
     nombres(g){
-        let result=false;
+        let resultado=false;
         for(let i=0;i<this.vertices.length;i++){
             if(this.vertices[i]==g.vertices[i]){
-                result=true;
-            }else {result=false;}
+                resultado=true;
+            }else {resultado=false;}
         }
 
-        return result;
+        return resultado;
     }
 
     pares(g){
-        let result=false;
+        let res=false;
         for(let i=0;i<this.numeroAristas;i++){
             for(let k=0;k<2;k++){
             if(this.conjuntos[i][k]===g.conjuntos[i][k]){
-                result=true;
-            }else {result=false;
+                res=true;
+            }else {res=false;
 }
             }}
         
-                return result;
+                return res;
 
     }
     isSubgraph(graph) {
@@ -89,40 +91,15 @@ class Graph {
     }
     isDirected()  //Saber si es un grafo dirigido u digrafo =>Tienen relacion con las matrices simetricas
     {
-        
+        let vertice= this.vertices
+        let Arr=[vertice]
+        for(i=0;i<Arr.length;i++){
+            
+        }
     }
 
 }
 
-var newgraph = new Graph(6);
-var ver = ['1', '2', '3', '4', '5', '6'];
-for (var n= 0; i < ver.length; n++) {
-    newgraph.addVertex(ver[n]);
-}
-
-newgraph.addEdge('1', '2');
-newgraph.addEdge('2', '3');
-newgraph.addEdge('3', '4');
-newgraph.addEdge('4', '1');
-newgraph.addEdge('1', '3');
-newgraph.addEdge('4', '2');
-newgraph.addEdge('3', '6');
-newgraph.addEdge('5', '6');
-
-newgraph.printGraph();
-console.log(newgraph.isComplete());
-console.log(newgraph.isMultigraph());
-/*
-Resultado:
-1 -> 2-4-3
-2 -> 1-3-4
-3 -> 2-4-1-6
-4 -> 3-1-2
-5 -> 6
-6 -> 3-5
-false
-false
-*/
 
 var grafo2 = new Graph(3);
 var vertices2 = ['1', '2', '3', '4'];
@@ -139,7 +116,7 @@ grafo2.addEdge('4', '2');
 grafo2.printGraph();
 console.log(grafo2.isComplete());
 console.log(grafo2.isMultigraph());
-console.log(grafo2.isSubgraph(newgraph));
+console.log(grafo2.isSubgraph(grafo));
 
 //Resultado:
 //1 -> 2-4-3
@@ -152,37 +129,6 @@ console.log(grafo2.isSubgraph(newgraph));
 
 
 
-var grafo3 = new Graph(2);
-var vertices3 = ['1', '2'];
-for (var i = 0; i < vertices3.length; i++) {
-    grafo3.addVertex(vertices3[i]);
-}
-grafo3.addEdge('1', '1');
-grafo3.addEdge('1', '2');
 
-grafo3.printGraph();
-console.log(grafo3.isComplete());
-console.log(grafo3.isMultigraph());
-console.log(grafo3.isSubgraph(newgraph));
 
-//1 -> 1-1-2
-//2 -> 1
-//false
-//true
 
-var grafo4= new Graph(4)
-var vertices4=['11','22','33','44']
-for(var e=0;e<vertices4.length;e++){
-    grafo4.addVertex(vertices4[e]);
-
-}
-grafo2.addEdge('11', '22');
-grafo2.addEdge('22', '33');
-grafo2.addEdge('33', '44');
-grafo2.addEdge('44', '11');
-grafo2.addEdge('11', '33');
-grafo2.addEdge('44', '22');
-grafo4.printGraph()
-console.log(grafo4.isComplete())
-console.log(grafo4.isMultigraph())
-console.log(grafo4.isSubgraph(newgraph))
